f8e9fae8493de1134e000e70ffd2e5b9ea902226
Manuj Garg <manuj.garg.cn@snapdeal.com>
2015-06-22 03:12:58 +0530

    jost do it

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,31 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This function returns a list of functions which can change the matrix and also store its inverse in cache
 makeCacheMatrix <- function(x = matrix()) {
-
+  my_matrix_inverse=NULL
+  set<-function(decoy){
+    my_matrix<<-decoy
+    my_matrix_inverse<<-NULL
+  }
+  get<-function() my_matrix
+  set_inverse<-function(solve) my_matrix_inverse<<-solve
+  get_inverse<-function() my_matrix_inverse
+  list(set=set,get=get,set_inverse=set_inverse,get_inverse=get_inverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  my_matrix_inverse <- my_matrix$get_inverse()
+  if(!is.null(my_matrix_inverse)) {
+    message("getting cached data")
+    return(my_matrix_inverse)
+  }
+  data <- my_matrix$get()
+  my_matrix_inverse <- solve(data, ...)
+  my_matrix$set_inverse(my_matrix_inverse)
+  my_matrix_inverse
         ## Return a matrix that is the inverse of 'x'
 }
